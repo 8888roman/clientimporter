081cd945bb5f9ee59c690655d9036ce90f80a492
8888roman <pawelrogozinski88@gmail.com>
2017-03-03 14:26:19 +0100

    Initial commit

---
+++ src/main/java/com/ing/ClientimporterApplication.java
@@ -0,0 +1,12 @@
+package com.ing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientimporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientimporterApplication.class, args);
+	}
+}
